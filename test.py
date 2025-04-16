db1b57900f35047670b1d021103156b96bf27e71
Abhinaykotla <abhinaykotla@gmail.com>
2025-04-16 01:27:35 -0500

    Remove unused test.py file to clean up the repository

--- test.py
+++
@@ -1,6 +0,0 @@
-from predict_lifespan import predict_poor_quality_age
-from mlp_nn import train_nn, test_nn
-from gnb import train_gnb_model 
-from bnb import train_bnb_model
-
-print("output of function:" ,train_nn()) 
